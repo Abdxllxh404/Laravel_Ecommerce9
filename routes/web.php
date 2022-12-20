f044ef3814b9e91bb2205ee94baa6c6627270ff5
Abdulloh404 <adlforgithub@gmail.com>
2022-12-20 20:44:29 +0700

    UPDATE_Livewire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\ContactUsComponent;
+use App\Http\Livewire\AboutUsComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CheckoutComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',HomeComponent::class)->name('home');
+Route::get('/about-us',AboutUsComponent::class)->name('aboutus');
+Route::get('/contact-us',ContactUsComponent::class)->name('contactus');
+Route::get('/cart',CartComponent::class)->name('cart');
+Route::get('/checkout',CheckoutComponent::class)->name('checkout');
+Route::get('/shop',ShopComponent::class)->name('shop');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
